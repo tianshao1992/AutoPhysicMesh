e0ee3fc6764896a98a710acb51fc2e1e96a1cc1d
tianshao1992 <tianyuannn@163.com>
2023-10-24 15:25:51 +0800

    add MultiDataloader!

---
+++ Module/FusionModule.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# @Copyright (c) 2023 Baidu.com, Inc. All Rights Reserved
+# @Time    : 2023/10/23 16:46
+# @Author  : Liu Tianyuan (liutianyuan02@baidu.com)
+# @File    : fusion_module.py
+# @Description    : ******
+"""
